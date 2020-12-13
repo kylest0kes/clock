ac64695dae8d1aa6dd2b92461c33d58c8f185792
kylest0kes <kylestokes1551@gmail.com>
2020-12-12 19:12:55 -0500

    created digital clock in JS. needs positioning and additional tweaks

--- script.js
+++ script.js
@@ -3,23 +3,53 @@ const hourHand = document.querySelector('.hour-hand');
 const minuteHand = document.querySelector('.min-hand');
 const secondHand = document.querySelector('.second-hand');
 
-function setDate() {
-    const now = new Date();
+function setAnalogTime() {
+    let now = new Date();
     
-    const hour = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
     
-    const hourDegrees = ((hour / 12) * 360) + 90;
-    const minutesDegrees = ((minutes / 60) * 360) + 90;
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    let hourDegrees = ((hour / 12) * 360) + 90;
+    let minutesDegrees = ((minutes / 60) * 360) + 90;
+    let secondsDegrees = ((seconds / 60) * 360) + 90;
     
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 };
 
-setInterval(setDate, 1000);
+// setInterval(setAnalogTime, 1000);
+
+function setDigitalTime() {
+    let now = new Date();
+    
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let dayOrNight = "AM";
+
+    if(hour == 0) {
+        hour = 12;
+    }
+    if(hour > 12) {
+        hour = hour - 12;
+        dayOrNight = "PM";
+    }
+
+    hour = (hour < 10)? "0" + hour : hour;
+    minutes = (minutes < 10)? "0" + minutes : minutes;
+    seconds = (seconds < 10)? "0" + seconds : seconds;
+
+    let time = hour + ":" + minutes + ":" + seconds + " " + dayOrNight;
+
+    document.getElementById("d-c").innerText = time;
+    document.getElementById("d-c").textContent = time;
+
+    setTimeout(setDigitalTime, 1000);
+}
+
+setDigitalTime();
 
 // TOGGLE FUNCTIONALITY
 document.querySelector('.theme-toggle').addEventListener('click', () => {
